e97f30dd2c4677e00116d6dfbac53c147f2180a1
Roman <tyniv@progroma.com>
2021-05-19 23:51:50 +0300

    learning useContext

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import './App.css';
+import Main from './Main';
 
-import ItemsList from './examples/itemsList'
 
 
 function App() {
- const [colored, setColored] = useState(false);
- const [count, setCount] = useState(1);
-
-const styles = {
-  color: colored ? 'darkred' : 'black'
-}
-
-const generateItemsFromAPI = useCallback( 
-  () => {
-    return new Array(count).fill('').map((_,i) => `Елемент ${i + 1}`)
-  }, [count]
-) 
-
+ 
  
 
 
   return (
-    <>
-       <h1 style={styles}> Кількість елементів: {count}</h1>
-       <button className={"btn btn-success"} 
-          onClick={ () => setCount( prev => prev + 1)}
-        > Добавити </button>
-        <button className={"btn btn-warning"} 
-          onClick={ () => setColored( prev => !prev)}
-        > Змінити </button>
-
-        <ItemsList getItems={generateItemsFromAPI} />
-    </>
+    <div className={'container'}>
+       <Main />
+    </div>
   )
 }
 
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+
+    return (
+        <>
+        <h1>Привіт у прикладі з Context</h1>
+        <button className="btn btn-success"> Показати alert!</button>
+        </>
+    )
+}
