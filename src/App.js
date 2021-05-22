a9c567fe55b7c9215b78c588ada3773cad3d5ff9
Roman <tyniv@progroma.com>
2021-05-22 18:10:57 +0300

    creating own hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import Main from './Main';
-import Alert from './alert/Alert';
-import {AlertProvider} from './alert/AlertContext'
+import React, { useEffect, useState } from 'react';
 
+
+function useLogger(value) {
+  useEffect( () => { 
+    if (value==='roman') {
+      console.log('Someone wrote roman')
+    }
+  }, [value])
+}
+
+function useInput(initialValue) {
+  const [value,setValue] = useState(initialValue);
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  return {
+    value, onChange
+  }
+
+}
 
 function App() {
+
+  const input = useInput('')
+  const lastName = useInput('')
+  useLogger(input.value);
+
   return (
-<AlertProvider>
-    <div div className={'container'}>
-      <Alert />
-      <Main toggle={ () => {} } />
+    <div div className={'container pt-3'}>
+      <input type="text" {...input} />
+      <input type="text" {...lastName} />
+      <hr />
+      <h1>{input.value} {lastName.value}</h1>
     </div>
-</AlertProvider> 
   )
 }
 
